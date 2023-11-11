05f1e293d6c98973a0d7905956eb9d0f24ee69e4
Marcos Gregorio <marquinhogreg2013@gmail.com>
2023-11-10 21:57:08 -0300

    ok

---
+++ interpreter/Interpreter.hs
@@ -0,0 +1,13 @@
+module Interpreter where
+
+import Lexer
+
+step:: Expr -> Expr
+step (Add (Num n1) (Num n2)) = Num (n1 + n2)
+step (Add (Num n) e) = Add (Num n) (step e)
+step (Add e1 e2) = Add (step e1) e2
+
+step (And BFalse _) = Bfalse
+step (And BTrue e) = e
+step (And e1 e2) = And (step e1) e2
+step e = e
---
+++ interpreter/Lexer.hs
@@ -0,0 +1,8 @@
+module Lexer where
+
+data Expr = BTrue
+  | BFalse
+  | Num Int
+  | Add Expr Expr
+  | And Expr Expr
+  deriving Show
